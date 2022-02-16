7f9d9657575a5823319a9b917f6dfe2dfe7ecb44
hm <hwill@acm.org>
2022-02-15 22:33:18 -0800

    WIP: Adding optimizer functions

---
+++ library/include/slammer/optimizer.h
@@ -0,0 +1,80 @@
+// BSD 3-Clause License
+//
+// Copyright (c) 2021, Hans-Martin Will
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef SLAMMER_OPTIMIZER_H
+#define SLAMMER_OPTIMIZER_H
+
+#pragma once
+
+#include "slammer/slammer.h"
+
+#include "Eigen/Sparse"
+
+namespace slammer {
+
+/// The signature of the function to compute the Jacobian that is passed to the least squares optimizer
+/// functions.
+typedef std::function<Eigen::SparseMatrix<double>(const Eigen::VectorXd& value)> Jacobian;
+
+/// The signature of the function to compute the Jacobian that is passed to the least squares optimizer
+/// functions.
+typedef std::function<Eigen::VectorXd(const Eigen::VectorXd& value)> Residual;
+
+/// Least-squares optimization using Gauss-Newton iteration.
+///
+/// \param jacobian_function    function that can calculate the Jaconbian for a given set of parameters values.
+/// \param residual_function    function that can calculate the residual vector for a given set of parameters values.
+/// \param value                the value to be optimized. It serves as initial value and is updated
+///                             as the optimization progresses.
+/// \param max_iterations       Maximum number of iterations to perform
+///
+/// \returns        the squared error associated with the value at the end of the optimization procedure, 
+///                 which is the sum of the squares of the residuals
+Result<double> GaussNewton(const Jacobian& jacobian_function, const Residual& residual_function, 
+                           Eigen::VectorXd& value, unsigned max_iterations);
+
+/// Least-squares optimization using Levenberg-Marquardt iteration.
+///
+/// \param jacobian_function    function that can calculate the Jaconbian for a given set of parameters values.
+/// \param residual_function    function that can calculate the residual vector for a given set of parameters values.
+/// \param value                the value to be optimized. It serves as initial value and is updated
+/// \param max_iterations        maximum number of iterations to perform
+/// \param lambda               the lambda parameter, which determines blending between Gauss-Newton and gradient 
+///                             descent
+///
+/// \returns        the squared error associated with the value at the end of the optimization procedure, 
+///                 which is the sum of the squares of the residuals
+Result<double> LevenbergMarquardt(const Jacobian& jacobian_function, const Residual& residual_function,
+                                  Eigen::VectorXd& value, unsigned max_iterations, 
+                                  double lambda);
+
+} // namespace slammer
+
+#endif //ndef SLAMMER_OPTIMIZER_H
---
+++ library/src/optimizer.cpp
@@ -0,0 +1,95 @@
+// BSD 3-Clause License
+//
+// Copyright (c) 2021, Hans-Martin Will
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "slammer/optimizer.h"
+
+#include "Eigen/SparseCholesky"
+
+using namespace slammer;
+
+Result<double> 
+slammer::GaussNewton(const Jacobian& jacobian_function, const Residual& residual_function,
+                     Eigen::VectorXd& value, unsigned max_iterations) {
+    while (max_iterations--) {
+        auto jacobian = jacobian_function(value);
+        auto residual = residual_function(value);
+        Eigen::SparseMatrix<double> lhs = jacobian.transpose() * jacobian;
+        Eigen::VectorXd rhs = jacobian.transpose() * residual;
+        Eigen::SimplicialLLT<Eigen::SparseMatrix<double> > solver;
+        solver.compute(lhs);
+
+        if(solver.info() != Eigen::Success) {
+            return Error("slammer::GaussNewton: Failed to factorize matrix");
+        }
+
+        value -= solver.solve(rhs);
+
+        if(solver.info() != Eigen::Success) {
+            return Error("slammer::GaussNewton: Failed to solve for right-hand side");
+        }
+
+    }
+
+    auto residual = residual_function(value);
+    return residual.squaredNorm();
+}
+
+Result<double> 
+slammer::LevenbergMarquardt(const Jacobian& jacobian_function, const Residual& residual_function,
+                            Eigen::VectorXd& value,
+                            unsigned max_iterations, double lambda) {    
+    while (max_iterations--) {
+        auto jacobian = jacobian_function(value);
+        auto dim = jacobian.rows();
+        Eigen::SparseMatrix<double> lambda_identity(dim, dim);
+        lambda_identity.setIdentity();
+        lambda_identity *= lambda;
+
+        auto residual = residual_function(value);
+        Eigen::SparseMatrix<double> lhs = jacobian.transpose() * jacobian + lambda_identity;
+        Eigen::VectorXd rhs = jacobian.transpose() * residual;
+        Eigen::SimplicialLLT<Eigen::SparseMatrix<double> > solver;
+        solver.compute(lhs);
+
+        if(solver.info() != Eigen::Success) {
+            return Error("slammer::LevenbergMarquardt: Failed to factorize matrix");
+        }
+
+        value -= solver.solve(rhs);
+
+        if(solver.info() != Eigen::Success) {
+            return Error("slammer::LevenbergMarquardt: Failed to solve for right-hand side");
+        }
+
+    }
+
+    auto residual = residual_function(value);
+    return residual.squaredNorm();
+}
